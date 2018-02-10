bfda6631001be29529a75fac9db388fe6844b490
Josh Humpherys <joshua.humpherys@gmail.com>
2018-02-10 10:11:28 -0500

    Abstract hello world method

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(helloWorld());
+    }
+
+    public static String helloWorld() {
+        return "Hello, world!";
     }
 }
